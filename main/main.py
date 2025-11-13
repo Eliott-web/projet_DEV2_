21e6533c66a4d098c38521d8f80a79aa90f5f915
Eliott-web <he202397@students.ephec.be>
2025-11-13 12:27:33 +0100

    création de l'affichage du score

--- main/main.py
+++ main/main.py
@@ -1,5 +1,12 @@
 from game_manager.actions_utils import initGamePlay
 from plateau.plateau_utils import *
+from score.score import Score
+
+
+joueur1 = Score("Eliott")
+
+joueur1.afficher_score()
+
 
 plat = makePlateau()
 path = makePath()
