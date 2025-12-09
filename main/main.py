f1bd75d925a8173761050d8acf83dc60dcd7f032
Eliott-web <he202397@students.ephec.be>
2025-12-09 10:15:32 +0100

    implémentation du dé dans le déroulement du jeu

--- main/main.py
+++ main/main.py
@@ -2,16 +2,17 @@ from game_manager.actions_utils import initGamePlay
 from plateau.plateau_utils import *
 from score.score import Score
 from game_manager.mini_game.jeux.garage import MiniGameGarage
+from plateau.die.die import lancer_de
 
 
 joueur1 = Score("Eliott")
-
+'''
 car = MiniGameGarage()
 print(car)
 
 car.start()
-
 '''
+
 plat = makePlateau()
 path = makePath()
 pion = makePion()
@@ -20,10 +21,10 @@ case = makeCase()
 pion.setScore(joueur1)
 
 #path.addCase(case)
-path = makePathLength(10)
+path = makePathLength(30)
 
 plat.addPath(path)
 plat.setPion(pion)
-movePion(plat, 0)
+step = lancer_de()
+movePion(plat, step)
 initGamePlay(plat)
-'''
--- main/plateau/die/die.py
+++ main/plateau/die/die.py
@@ -4,4 +4,4 @@ import random
 def lancer_de():
     return random.randint(1, 6)
 
-print('Tu as obtenu :', lancer_de())
+#print('Tu as obtenu :', lancer_de())
