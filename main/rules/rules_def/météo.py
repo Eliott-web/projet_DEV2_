4ac391e90f09ed622cdb8cba7cbe1af86507d021
Rayan K <r.kinet@students.ephec.be>
2025-12-11 19:44:26 +0100

    rules

--- main/rules/rules_def/météo.py
+++ main/rules/rules_def/météo.py
@@ -7,15 +7,19 @@ class Orage(Rule):
 
     def __init__(self):
         super().__init__("Orage", "tout le monde perds des points" )
-
+        self.points_perdu = 0
 
     def onAdd(self):
         perte = random.randint(1,10)
-        point_perdu = min(perte, Score.basePoints)
-        super().on_add(point_perdu)
-
-
+        self.points_perdu = min(perte, Score.basePoints)
+        Score.basePoints -= self.points_perdu
+        super().on_add()
     def onRemove(self):
-        pass
+
+        Score.basePoints -= self.points_perdu
+        self.points_perdu = 0
+        super().on_remove()
+
+
 
 
