845d2dfa613fcaad374589811df52b1aaeeaf17d
Rayan K <r.kinet@students.ephec.be>
2025-12-16 16:41:14 +0100

    rules

--- main/rules/rules_def/de_maudit.py
+++ main/rules/rules_def/de_maudit.py
@@ -2,13 +2,13 @@ from main.plateau.die import die
 from main.rules.rule import Rule
 
 
-class MalusDe(Rule):
+class de_mauidit(Rule):
     def __init__(self):
-        super().__init__("Malus Dé", "Le destin vous immobilise… Tous vos lancers de dé donnent 0.")
+        super().__init__("dé maudit", "Chaque lancer de dé vous fait reculer")
 
     def on_add(self):
         # On force le dé à faire 1
-        die.multiplicateur = 0
+        die.multiplicateur = -1
         super().on_add()
 
     def on_remove(self):
